a69d6d02bffa1ae3b919a9a1c8676473977dbb31
knowave <kwaktaemin98@gmail.com>
2023-05-26 15:48:52 +0900

    nest.js mongodb study

--- nestjs_mongodb/src/cat/cat.controller.ts
+++ nestjs_mongodb/src/cat/cat.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CatService } from './cat.service';
+import { createCatDto } from './dto/create-cat.dto';
+import { Cat } from './schemas/cat.schema';
 
 @Controller('cat')
-export class CatController {}
+export class CatController {
+  constructor(private readonly catService: CatService) {}
+
+  @Post()
+  async create(@Body() createCatDto: createCatDto): Promise<Cat> {
+    return await this.catService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return await this.catService.findAll();
+  }
+}
