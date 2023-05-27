0be396bf527d79a6e6fcfcbc7977c5a8a0a44b7b
knowave <kwaktaemin98@gmail.com>
2023-05-27 12:17:02 +0900

    Add Cron

--- nestjs_logging/src/cat/cat.module.ts
+++ nestjs_logging/src/cat/cat.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CatService } from './cat.service';
+import { LoggerModule } from 'src/logger/logger.module';
+import { MyLogger } from 'src/logger/my-logger.service';
 
 @Module({
-  providers: [CatService]
+  imports: [LoggerModule],
+  providers: [CatService, MyLogger],
 })
 export class CatModule {}
--- nestjs_logging/src/cat/cat.service.ts
+++ nestjs_logging/src/cat/cat.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { Cat } from './dto/cat.dto';
 import { MyLogger } from 'src/logger/my-logger.service';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
-export class CatService {
+export class CatService implements OnApplicationBootstrap {
   private readonly cats: Cat[] = [];
 
   constructor(private myLogger: MyLogger) {
     this.myLogger.setContext('CatService');
   }
 
+  onApplicationBootstrap() {
+    this.findAll();
+  }
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
   findAll(): Cat[] {
     try {
       const findAllCats: Cat[] = [];
@@ -23,7 +29,6 @@ export class CatService {
       findAllCats.push(cat);
 
       this.myLogger.customLog();
-
       return findAllCats;
     } catch (err) {
       this.myLogger.warn(`Cats Not Found : ${err}`);
