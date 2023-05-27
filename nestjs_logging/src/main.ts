f50898d8378d0bd7be52d773c75ad8da891966c8
knowave <kwaktaemin98@gmail.com>
2023-05-27 12:17:37 +0900

    Use Logger

--- nestjs_logging/src/main.ts
+++ nestjs_logging/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MyLogger } from './logger/my-logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(new MyLogger());
   await app.listen(3000);
 }
 bootstrap();
