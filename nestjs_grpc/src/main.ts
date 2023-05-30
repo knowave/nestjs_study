887374a197bc60b3e3b75be291f414b743620ba7
knowave <kwaktaemin98@gmail.com>
2023-05-30 19:18:29 +0900

    connect microservice

--- nestjs_grpc/src/main.ts
+++ nestjs_grpc/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      options: {
+        package: 'hero',
+        protoPath: join(__dirname, 'hero/hero.proto'),
+      },
+    },
+  );
   await app.listen(3000);
 }
 bootstrap();
