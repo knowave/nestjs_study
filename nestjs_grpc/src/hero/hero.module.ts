df45c200297d5882321f9c5395bfedf89dbe46b1
knowave <kwaktaemin98@gmail.com>
2023-05-31 14:57:33 +0900

    Add gRPC Client Module

--- nestjs_grpc/src/hero/hero.module.ts
+++ nestjs_grpc/src/hero/hero.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { HeroController } from './hero.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
-  controllers: [HeroController]
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'HERO_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'hero',
+          protoPath: join(__dirname, 'hero/hero.proto'),
+        },
+      },
+    ]),
+  ],
+  controllers: [HeroController],
 })
 export class HeroModule {}
