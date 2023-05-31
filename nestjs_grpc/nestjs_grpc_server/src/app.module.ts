555c7bbd3b917985ab9eca734fa60b94ba1a6464
knowave <kwaktaemin98@gmail.com>
2023-05-31 17:15:07 +0900

    gRPC 사용으로 REST 관련 Class 삭제

--- nestjs_grpc/nestjs_grpc_server/src/app.module.ts
+++ nestjs_grpc/nestjs_grpc_server/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HeroModule } from './hero/hero.module';
 
 @Module({
   imports: [HeroModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
