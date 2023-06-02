ce9f5050f044eed09612521d81805a69ac29ca8d
knowave <kwaktaemin98@gmail.com>
2023-06-02 12:32:38 +0900

    AddMessageQueue

--- nestjs_queues/src/app.controller.ts
+++ nestjs_queues/src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('queue')
+  addMessageQueue(@Body('data') data: string) {
+    return this.addMessageQueue(data);
+  }
 
   @Get()
   getHello(): string {
--- nestjs_queues/src/app.service.ts
+++ nestjs_queues/src/app.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('messageQueue') private messageQueue: Queue) {}
+
+  async addMessageQueue(data: string) {
+    const job = await this.messageQueue.add({
+      data: data,
+    });
+
+    return job;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
