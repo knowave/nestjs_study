64637479e0714e7bf5534c0a7f61e92c708da302
knowave <kwaktaemin98@gmail.com>
2023-06-04 21:18:11 +0900

    create cats entity

---
+++ nestjs_testing/src/cats/entities/cats.entity.ts
@@ -0,0 +1,10 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('cats')
+export class Cats extends BaseEntity {
+  @PrimaryGeneratedColumn({ name: 'cat_id', comment: '고양이 고유값' })
+  catsId: number;
+
+  @Column('varchar', { name: 'name', comment: '고양이 이름' })
+  name: string;
+}
