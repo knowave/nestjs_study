1b0d9a040ba97a75769b85f2e63a17fcd77daade
knowave <kwaktaemin98@gmail.com>
2023-06-04 21:28:14 +0900

    import config module

--- nestjs_testing/src/app.module.ts
+++ nestjs_testing/src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { CatsModule } from './cats/cats.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
