6e59fce9dc62d2df72420972d45e080e1b433e65
knowave <kwaktaemin98@gmail.com>
2023-06-04 21:21:20 +0900

    database config

--- nestjs_testing/src/database/database.module.ts
+++ nestjs_testing/src/database/database.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cats } from 'src/cats/entities/cats.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Cats],
+        synchronize: true,
+      }),
+    }),
+  ],
+})
 export class DatabaseModule {}
