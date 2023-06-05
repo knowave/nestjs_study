e2ff5e394a6c661c63bfaae54c3acf16c413a5c1
knowave <kwaktaemin98@gmail.com>
2023-06-05 15:48:06 +0900

    Add CatsRepository

--- nestjs_testing/src/cats/cats.module.ts
+++ nestjs_testing/src/cats/cats.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { CatsRepository } from './cats.repository';
 
 @Module({
-  providers: [CatsService],
-  controllers: [CatsController]
+  providers: [CatsService, CatsRepository],
+  controllers: [CatsController],
 })
 export class CatsModule {}
