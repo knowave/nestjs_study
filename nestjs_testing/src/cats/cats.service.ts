bfc7cd7e0671e7ff09bab7189b30d6b628b994f2
knowave <kwaktaemin98@gmail.com>
2023-06-05 15:17:59 +0900

    비지니스 로직

--- nestjs_testing/src/cats/cats.service.ts
+++ nestjs_testing/src/cats/cats.service.ts
@@ -1,4 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CatsRepository } from './cats.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MyPaginationQuery } from 'src/base/pagination-query';
+import { Pagination, paginate } from 'nestjs-typeorm-paginate';
+import { Cats } from './entities/cats.entity';
+import { CATS_EXCEPTION } from 'src/exceptions/error-code';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  constructor(
+    @InjectRepository(CatsRepository) private catsRepository: CatsRepository,
+  ) {}
+
+  /**
+   * 고양이 전체 조회
+   * @param options
+   * @returns
+   */
+  async findAllByCats(options: MyPaginationQuery): Promise<Pagination<Cats>> {
+    return paginate(await this.catsRepository, options);
+  }
+
+  /**
+   * 특정 고양이 조회
+   * @param catsId
+   * @returns
+   */
+  async findOneById(catsId: number): Promise<Cats> {
+    const cat = await this.catsRepository.findOneById(catsId);
+
+    if (!cat) {
+      throw new NotFoundException(CATS_EXCEPTION.CAT_NOT_FOUND);
+    }
+
+    return cat;
+  }
+
+  /**
+   * 고양이 생성
+   * @param createCatDto
+   * @returns
+   */
+  async createCat(createCatDto: CreateCatDto): Promise<Cats> {
+    return await this.catsRepository.createCat(createCatDto);
+  }
+
+  /**
+   * 고양이 수정
+   * @param catsId
+   * @returns
+   */
+  async update(catsId: number) {
+    const cat = await this.findOneById(catsId);
+
+    return await this.catsRepository.update(catsId, { name: cat.name });
+  }
+
+  /**
+   * 고양이 삭제
+   * @param catsId 
+   * @returns 
+   */
+  async delete(catsId: number) {
+    const cat = await this.findOneById(catsId);
+
+    return await this.catsRepository.delete(cat.catsId);
+  }
+}
