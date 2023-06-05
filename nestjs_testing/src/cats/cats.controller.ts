989850833ab1488382776d51acea53a6371bd394
knowave <kwaktaemin98@gmail.com>
2023-06-05 15:48:23 +0900

    controller

--- nestjs_testing/src/cats/cats.controller.ts
+++ nestjs_testing/src/cats/cats.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { MyPaginationQuery } from 'src/base/pagination-query';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { Cats } from './entities/cats.entity';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Get('')
+  async findAllByCats(
+    @Query() options: MyPaginationQuery,
+  ): Promise<Pagination<Cats>> {
+    return await this.catsService.findAllByCats(options);
+  }
+
+  @Get(':catsId')
+  async findOneById(@Param('catsId') catsId: number): Promise<Cats> {
+    return await this.catsService.findOneById(catsId);
+  }
+
+  @Post('')
+  async createCat(@Body() createCatDto: CreateCatDto): Promise<Cats> {
+    return await this.catsService.createCat(createCatDto);
+  }
+
+  @Patch(':catsId')
+  async update(
+    @Param('catsId') catsId: number,
+    @Body() cats: Cats,
+  ): Promise<Cats> {
+    return await this.catsService.update(catsId, cats);
+  }
+
+  @Delete(':catsId')
+  async delete(@Param('catsId') catsId: number): Promise<void> {
+    return await this.catsService.delete(catsId);
+  }
+}
