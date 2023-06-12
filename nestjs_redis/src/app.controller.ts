1184f4421edf988babbf98e4eeec838ed560280f
knowave <kwaktaemin98@gmail.com>
2023-06-12 17:20:11 +0900

    Add CacheInterceptor

--- nestjs_redis/src/app.controller.ts
+++ nestjs_redis/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  CacheInterceptor,
+  Controller,
+  Get,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
+@UseInterceptors(CacheInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findAll(): String[] {
+    return [];
   }
 }
