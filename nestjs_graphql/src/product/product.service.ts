2aa9b620ee482ecc5559b5270fb759ca7ecc2604
knowave <kwaktaemin98@gmail.com>
2023-06-13 15:10:24 +0900

    service logic

--- nestjs_graphql/src/product/product.service.ts
+++ nestjs_graphql/src/product/product.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { Repository } from 'typeorm';
+import { ProductDto } from './dto/product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(@InjectRepository(Product) private readonly productRepository: Repository<Product>) {}
+
+    async createProduct(product: ProductDto): Promise<Product> {
+        const create = this.productRepository.create(product);
+
+        return await this.productRepository.save(create);
+    }
+
+    async findAllProducts(): Promise<Product[]> {
+        return await this.productRepository.find();
+    }
+}
