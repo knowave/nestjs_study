1d739d8c912f32af6894b8b30894b26a753e8df2
knowave <kwaktaemin98@gmail.com>
2023-06-23 15:53:50 +0900

    Sentry DSN init & intercepter global

--- nestjs_sentry/src/main.ts
+++ nestjs_sentry/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
+import { SentryIntercepter } from './common/sentry.intercepter';
+import { WebhookIntercepter } from './common/webhook.intercepter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
+  app.useGlobalInterceptors(new SentryIntercepter());
+  app.useGlobalInterceptors(new WebhookIntercepter());
   await app.listen(3000);
 }
 bootstrap();
