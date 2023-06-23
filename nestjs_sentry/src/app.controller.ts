4a9ea08c23dbb3c8e4bc46e16147e37cbb583101
knowave <kwaktaemin98@gmail.com>
2023-06-23 15:50:20 +0900

    sendError method

--- nestjs_sentry/src/app.controller.ts
+++ nestjs_sentry/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/error')
+  sendError(): string {
+    return this.appService.sendError();
+  }
 }
--- nestjs_sentry/src/app.service.ts
+++ nestjs_sentry/src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  sendError(): string {
+    throw new BadRequestException('ERROR!!');
   }
 }
