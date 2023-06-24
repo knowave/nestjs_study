38273162d0b771bab516f57fd4dfee2f3c1d91fc
knowave <kwaktaemin98@gmail.com>
2023-06-24 23:50:30 +0900

    s3 upload

--- nestjs_file_upload/src/upload/upload.controller.ts
+++ nestjs_file_upload/src/upload/upload.controller.ts
@@ -5,12 +5,34 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import * as AWS from 'aws-sdk';
 
 @Controller('upload')
 export class UploadController {
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+
+    try {
+      const upload = await new AWS.S3()
+        .putObject({
+          Key: `${Date.now() + file.originalname}`,
+          Body: file.buffer,
+          Bucket: process.env.AWS_S3_BUKET,
+        })
+        .promise();
+
+      console.log(file);
+      console.log(upload);
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
