0d2694cc5add415fda62fd16c056d78d269a9683
Taemin <kwaktaemin98@gmail.com>
2023-07-31 17:26:47 +0900

    posts module

--- nestjs_file_upload/src/posts/posts.module.ts
+++ nestjs_file_upload/src/posts/posts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { UsersService } from 'src/users/users.service';
+import { UploadService } from 'src/upload/upload.service';
 
 @Module({
+  imports: [UsersService, UploadService],
   providers: [PostsService],
-  controllers: [PostsController]
+  controllers: [PostsController],
 })
 export class PostsModule {}
