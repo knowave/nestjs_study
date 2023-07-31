ad389e16db48f22c95a153bb678087ca087296e9
Taemin <kwaktaemin98@gmail.com>
2023-07-31 18:55:51 +0900

    Add Swagger

--- nestjs_file_upload/src/main.ts
+++ nestjs_file_upload/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('nestjs_upload')
+    .setDescription('The NestJS API Description')
+    .setVersion('1.0')
+    .addTag('AWS S3 & Multer')
+    .build();
+  const docs = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, docs);
+
   await app.listen(3000);
 }
 bootstrap();
