54331eadabc9c6d6b162837717a3b93292370946
Taemin <kwaktaemin98@gmail.com>
2023-07-31 17:56:44 +0900

    경로 수정

--- nestjs_file_upload/src/posts/posts.module.ts
+++ nestjs_file_upload/src/posts/posts.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { UsersService } from 'src/users/users.service';
-import { UploadService } from 'src/upload/upload.service';
+import { UsersService } from '../users/users.service';
+import { UploadService } from '../upload/upload.service';
+import { PostsRepository } from './posts.repository';
 
 @Module({
   imports: [UsersService, UploadService],
-  providers: [PostsService],
+  providers: [PostsService, PostsRepository],
   controllers: [PostsController],
 })
 export class PostsModule {}
