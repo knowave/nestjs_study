45a4f4d3e3ead8dfa137e85050985fc9e0c03977
Taemin <knowave@github.com>
2023-07-31 15:22:04 +0900

    Add DB Configuration

--- nestjs_file_upload/src/app.module.ts
+++ nestjs_file_upload/src/app.module.ts
@@ -1,15 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UploadModule } from './upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+import { DataBaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
     }),
+    DataBaseModule,
+    UsersModule,
+    PostsModule,
+    AuthModule,
     UploadModule,
   ],
   controllers: [AppController],
