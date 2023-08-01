91e280ba2f51cef7a31ce1e4f7a9c8409316f3e2
Taemin <kwaktaemin98@gmail.com>
2023-08-01 17:12:38 +0900

    imports TypeOrmModule

--- nestjs_file_upload/src/posts/posts.module.ts
+++ nestjs_file_upload/src/posts/posts.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { UsersService } from '../users/users.service';
-import { UploadService } from '../upload/upload.service';
 import { PostsRepository } from './posts.repository';
+import { UsersModule } from '../users/users.module';
+import { UploadModule } from '../upload/upload.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from '../entities/post.entity';
 
 @Module({
-  imports: [UsersService, UploadService],
+  imports: [
+    TypeOrmModule.forFeature([Post]),
+    UsersModule, 
+    UploadModule,
+  ],
   providers: [PostsService, PostsRepository],
   controllers: [PostsController],
 })
