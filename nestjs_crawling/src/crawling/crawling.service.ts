44eb9134f79d84985b9673617f6b267ac9c99866
knowave <kwaktaemin98@gmail.com>
2023-08-05 23:55:48 +0900

    크롤링 서비스 구현 중

--- nestjs_crawling/src/crawling/crawling.service.ts
+++ nestjs_crawling/src/crawling/crawling.service.ts
@@ -1,4 +1,32 @@
+// crawling.service.ts
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 @Injectable()
-export class CrawlingService {}
+export class CrawlingService {
+  constructor() {}
+
+  async crawlDataFromWebpage(url: string): Promise<any> {
+    try {
+      const response = await axios.get(url);
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      // 웹 페이지에서 필요한 데이터 추출
+      const data = [];
+      $('selector-for-target-elements').each((index, element) => {
+        const item = $(element).text();
+        data.push(item);
+      });
+
+      console.log(response);
+
+      return data;
+    } catch (error) {
+      throw new Error('크롤링에 실패하였습니다.');
+    }
+  }
+}
