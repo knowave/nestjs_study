5a3cf300f0ef380b32bc5bfd34fdc39d730aaa1b
Taemin <kwaktaemin98@gmail.com>
2023-08-14 10:48:17 +0900

    users service unit test defined

--- unit_test/src/users/users.service.spec.ts
+++ unit_test/src/users/users.service.spec.ts
@@ -1,15 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { DataSource, QueryRunner, Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+const mockRepository = () => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
+
+const mockQueryRunner = {
+  manager: {},
+} as QueryRunner;
+
+class MockDataSource {
+  createQueryRunner(): QueryRunner {
+    return mockQueryRunner;
+  }
+}
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UsersService', () => {
   let service: UsersService;
+  let dataSource: DataSource;
+  let repository: MockRepository<User>;
 
   beforeEach(async () => {
+    Object.assign(mockQueryRunner.manager, {
+      create: jest.fn(),
+      save: jest.fn(),
+    });
+
+    mockQueryRunner.connect = jest.fn();
+    mockQueryRunner.startTransaction = jest.fn();
+    mockQueryRunner.commitTransaction = jest.fn();
+    mockQueryRunner.rollbackTransaction = jest.fn();
+    mockQueryRunner.release = jest.fn();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: DataSource,
+          useClass: MockDataSource,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<MockRepository<User>>(getRepositoryToken(User));
+    dataSource = module.get<DataSource>(DataSource);
   });
 
   it('should be defined', () => {
