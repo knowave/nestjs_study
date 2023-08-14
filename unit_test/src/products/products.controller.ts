d59e7f500d16a3bf455a09705c0d7819a9cdfb66
Taemin <kwaktaemin98@gmail.com>
2023-08-14 11:22:35 +0900

    controller implement

--- unit_test/src/products/products.controller.ts
+++ unit_test/src/products/products.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { GetUser } from 'src/decorators/user.decorator';
+import { User } from 'src/entities/user.entity';
+import { ProductResponseDto } from './dto/product-response.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  async createProduct(
+    @Body() { name, description }: CreateProductDto,
+    @GetUser() user: User,
+  ): Promise<ProductResponseDto> {
+    return await this.productsService.createProduct(
+      {
+        name,
+        description,
+      },
+      user,
+    );
+  }
+
+  @Get('/:id')
+  async getProductById(@Param() id: number): Promise<ProductResponseDto> {
+    return await this.productsService.getProductById(id);
+  }
+}
