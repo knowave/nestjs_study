95cf7901085bde34509f68fd34c0b0eae4cd7ee3
Taemin <kwaktaemin98@gmail.com>
2023-08-14 10:46:46 +0900

    users controller implement

--- unit_test/src/users/users.controller.ts
+++ unit_test/src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserResponseDto } from './dto/user-response.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(
+    @Body() { email, firstName, secondName }: CreateUserDto,
+  ): Promise<UserResponseDto> {
+    return await this.usersService.createUser({
+      email,
+      firstName,
+      secondName,
+    });
+  }
+
+  @Get('/:id')
+  async getUserById(@Param() id: number): Promise<UserResponseDto> {
+    return await this.usersService.getUserById(id);
+  }
+}
