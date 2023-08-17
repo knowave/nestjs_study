ccd74c0ade22c036ea944a7718557afbf6b05947
Taemin <kwaktaemin98@gmail.com>
2023-08-17 22:58:26 +0900

    image url, category, title 크롤링

--- nestjs_crawling/src/crawling/crawling.service.ts
+++ nestjs_crawling/src/crawling/crawling.service.ts
@@ -15,15 +15,16 @@ export class CrawlingService {
       const $ = cheerio.load(html);
 
       // 웹 페이지에서 필요한 데이터 추출
-      const data = [];
-      $('.cont_thumb').each((index, element) => {
-        const image = $(element).find('.info_thumb').text();
-        console.log('image: ', image);
-        const item = $(element).text();
-        data.push(item.trim());
+      const newsItems = [];
+      $('.item_issue').each((index, element) => {
+        const image = $(element).find('.wrap_thumb img').attr('src');
+        const category = $(element).find('.txt_category').text().trim();
+        const title = $(element).find('.tit_g a').text().trim();
+
+        newsItems.push({ image, category, title });
       });
 
-      return data;
+      return newsItems;
     } catch (error) {
       throw new Error('크롤링에 실패하였습니다.');
     }
