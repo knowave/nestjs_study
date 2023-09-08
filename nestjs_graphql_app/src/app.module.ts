a0dd770846655697d5b4d49f967ed55156ab0dbf
knowave <kwaktaemin98@gmail.com>
2023-09-08 14:56:49 +0900

    GraphQL Setting

--- nestjs_graphql_app/src/app.module.ts
+++ nestjs_graphql_app/src/app.module.ts
@@ -4,6 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DataBaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import GraphQLJSON from 'graphql-type-json';
+import { Context } from 'apollo-server-core';
 import * as Joi from 'joi';
 
 @Module({
@@ -17,6 +21,22 @@ import * as Joi from 'joi';
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
       }),
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      resolvers: { JSON: GraphQLJSON },
+      playground: true,
+      autoSchemaFile: true,
+      installSubscriptionHandlers: true,
+      subscriptions: {
+        'graphql-ws': {
+          onConnect: (context: Context<any>) => {
+            const { connectionParams, extra } = context;
+            extra.token = connectionParams['jwt'];
+          },
+        },
+      },
+      context: ({ req, res }) => ({ req, res }),
     }),
     UserModule,
     DataBaseModule,
