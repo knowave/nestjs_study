c48805b94442398e229aef42764bbb397b693350
Taemin <kwaktaemin98@gmail.com>
2023-09-11 16:58:57 +0900

    추가: 팔로우/팔로잉 도메인 추가

---
+++ nestjs_graphql_app/src/follow/entities/follow.entity.ts
@@ -0,0 +1,38 @@
+import { ObjectType, Field, Int, InputType } from '@nestjs/graphql';
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { Gym } from 'src/gym/entities/gym.entity';
+import { Trainer } from 'src/trainer/entities/trainer.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Entity, ManyToOne } from 'typeorm';
+
+@InputType('followInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Follow extends BaseEntity {
+  @Field(() => User, { nullable: true })
+  @ManyToOne(() => User, (user) => user.follow, {
+    eager: true,
+    nullable: true,
+    onDelete: 'CASCADE',
+    cascade: ['soft-remove'],
+  })
+  user?: User;
+
+  @Field(() => Gym, { nullable: true })
+  @ManyToOne(() => Gym, (gym) => gym.follow, {
+    eager: true,
+    nullable: true,
+    onDelete: 'CASCADE',
+    cascade: ['soft-remove'],
+  })
+  gym?: Gym;
+
+  @Field(() => Trainer, { nullable: true })
+  @ManyToOne(() => Trainer, (trainer) => trainer.follow, {
+    eager: true,
+    nullable: true,
+    onDelete: 'CASCADE',
+    cascade: ['soft-remove'],
+  })
+  trainer?: Trainer;
+}
