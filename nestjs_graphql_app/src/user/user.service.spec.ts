0a0604085ea9f3f296355b2d9c959e9ef5a2c587
Taemin <kwaktaemin98@gmail.com>
2023-09-12 15:36:47 +0900

    추가: user service unit testing

---
+++ nestjs_graphql_app/src/user/user.service.spec.ts
@@ -0,0 +1,105 @@
+import { DataSource, QueryRunner, Repository } from 'typeorm';
+import { UserService } from './user.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { createConnection } from 'mysql2';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserInput } from './dto/create-user.dto';
+
+const mockRepository = () => ({
+  findOne: jest.fn(),
+  find: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+  softRemove: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+describe('UserService', () => {
+  let userService: UserService;
+  let userRepository: MockRepository<User>;
+  let dataSource: DataSource;
+
+  const mockQueryRunner = {
+    manager: {},
+  } as QueryRunner;
+
+  class MockDataSource {
+    createQueryRunner(mode?: 'master' | 'slave'): QueryRunner {
+      return mockQueryRunner;
+    }
+  }
+
+  beforeEach(async () => {
+    Object.assign(mockQueryRunner.manager, {
+      create: jest.fn(),
+      save: jest.fn(),
+      softRemove: jest.fn(),
+    });
+
+    mockQueryRunner.connect = jest.fn();
+    mockQueryRunner.startTransaction = jest.fn();
+    mockQueryRunner.commitTransaction = jest.fn();
+    mockQueryRunner.rollbackTransaction = jest.fn();
+    mockQueryRunner.release = jest.fn();
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: DataSource,
+          useClass: MockDataSource,
+        },
+        {
+          provide: createConnection,
+          useValue: jest.fn(),
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository(),
+        },
+      ],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+    userRepository = module.get<MockRepository<User>>(getRepositoryToken(User));
+    dataSource = module.get<DataSource>(DataSource);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('CreateUser', () => {
+    it('유저 생성 성공 시 transaction commit을 할 것이고, rollback은 하지 않는다.', async () => {
+      const mockCreateUser: CreateUserInput = {
+        email: 'test@test.com',
+        username: 'tester',
+        password: 'test1234@',
+      };
+
+      const queryRunner = dataSource.createQueryRunner();
+
+      jest
+        .spyOn(queryRunner.manager, 'create')
+        .mockReturnValue([mockCreateUser]);
+
+      jest.spyOn(queryRunner.manager, 'save').mockResolvedValue(mockCreateUser);
+
+      const result = await userService.createUser(mockCreateUser);
+
+      expect(queryRunner.manager.save).toHaveBeenCalledTimes(1);
+
+      console.log('result: ', result);
+
+      expect(queryRunner.connect).toHaveBeenCalledTimes(1);
+      expect(queryRunner.startTransaction).toHaveBeenCalledTimes(1);
+      expect(queryRunner.commitTransaction).toHaveBeenCalledTimes(1);
+      expect(queryRunner.rollbackTransaction).toHaveBeenCalledTimes(0);
+      expect(queryRunner.release).toHaveBeenCalledTimes(1);
+
+      expect(result).toEqual({ ok: true });
+    });
+  });
+});
