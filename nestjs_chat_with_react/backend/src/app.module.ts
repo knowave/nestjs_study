d0b38fbafa3fc9b69f697b777f975d0342c29a4e
Taemin <kwaktaemin98@gmail.com>
2023-10-24 11:56:13 +0900

    추가: 새로 생성된 module 추가

--- nestjs_chat_with_react/backend/src/app.module.ts
+++ nestjs_chat_with_react/backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './message/message.module';
+import { ChatGateway } from './chat/chat.gateway';
+import { ChatModule } from './chat/chat.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, MessageModule, ChatModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ChatGateway],
 })
 export class AppModule {}
