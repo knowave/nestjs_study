746c30f5dd88deaae13c3708eba1e54707386578
Taemin <kwaktaemin98@gmail.com>
2023-10-24 10:27:25 +0900

    추가: TypeOrmModule, userRepository 추가

--- nestjs_chat_with_react/backend/src/user/user.module.ts
+++ nestjs_chat_with_react/backend/src/user/user.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserRepository } from './user.repository';
 
 @Module({
-  providers: [UserService]
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
