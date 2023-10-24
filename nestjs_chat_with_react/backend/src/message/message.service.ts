7b25a4c29a6d8ae71db0942de1bd4d56520f7a84
Taemin <kwaktaemin98@gmail.com>
2023-10-24 10:47:12 +0900

    추가: messageRepostiory 메서드 추가

--- nestjs_chat_with_react/backend/src/message/message.service.ts
+++ nestjs_chat_with_react/backend/src/message/message.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Message } from './entities/message.entity';
+import { MessageRepository } from './message.repository';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { SendMessageDto } from './dto/send-message.dto';
 
 @Injectable()
-export class MessageService {}
+export class MessageService {
+  constructor(
+    @InjectRepository(Message)
+    private readonly messageRepository: MessageRepository,
+  ) {}
+
+  async createChat(createChatDto: CreateChatDto): Promise<Message> {
+    return await this.messageRepository.createChat(createChatDto);
+  }
+
+  async sendMessage(sendMessageDto: SendMessageDto): Promise<Message> {
+    return await this.messageRepository.sendMessage(sendMessageDto);
+  }
+
+  async markAsRead(messageId: number): Promise<void> {
+    return await this.messageRepository.markAsRead(messageId);
+  }
+}
