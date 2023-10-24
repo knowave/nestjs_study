5e3514e4092cc755862928f6f295d4b4c2a48e1d
Taemin <kwaktaemin98@gmail.com>
2023-10-24 11:03:01 +0900

    추가: message 생성, 전송, 읽음 확인 추가

--- nestjs_chat_with_react/backend/src/chat/chat.gateway.ts
+++ nestjs_chat_with_react/backend/src/chat/chat.gateway.ts
@@ -1,9 +1,57 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { MessageService } from 'src/message/message.service';
+import { UserService } from 'src/user/user.service';
 
 @WebSocketGateway()
 export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  constructor(
+    private readonly chatService: MessageService,
+    private readonly userService: UserService,
+  ) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('createChat')
+  async handleCreateChat(
+    client: any,
+    payload: { senderId: number; receiverId: number },
+  ) {
+    const sender = await this.userService.getUserById(payload.senderId);
+    const receiver = await this.userService.getUserById(payload.receiverId);
+
+    if (sender && receiver) {
+      const chat = await this.chatService.createChat({ sender, receiver });
+      client.emit('chatCreated', chat);
+    }
+  }
+
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(
+    client: any,
+    payload: { senderId: number; receiverId: number; content: string },
+  ) {
+    const sender = await this.userService.getUserById(payload.senderId);
+    const receiver = await this.userService.getUserById(payload.receiverId);
+    const content = payload.content;
+
+    if (sender && receiver) {
+      const message = await this.chatService.sendMessage({
+        sender,
+        receiver,
+        content,
+      });
+      client.emit('messageSend', message);
+    }
+  }
+
+  @SubscribeMessage('markAsRead')
+  async handleMarkAsRead(client: any, messageId: number) {
+    await this.chatService.markAsRead(messageId);
   }
 }
