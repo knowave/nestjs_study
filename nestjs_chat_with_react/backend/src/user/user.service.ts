f14bf0b782093b0f00da5688fa328bb882fe2433
Taemin <kwaktaemin98@gmail.com>
2023-10-24 10:27:00 +0900

    생성: user service 생성

--- nestjs_chat_with_react/backend/src/user/user.service.ts
+++ nestjs_chat_with_react/backend/src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    return await this.userRepository.createUser(createUserDto);
+  }
+
+  async getUserById(userId: number): Promise<User> {
+    return await this.userRepository.getUserById(userId);
+  }
+}
