530cd1a63874f1936e8013b04d08b5a41643fce6
Taemin <kwaktaemin98@gmail.com>
2023-11-02 11:21:28 +0900

    authModule 추가

--- nestjs_social_login/src/auth/auth.module.ts
+++ nestjs_social_login/src/auth/auth.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/user/user.module';
+import { User } from 'src/user/entities/user.entity';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { KakaoStrategy } from './strategies/kakao.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { NaverStrategy } from './strategies/naver.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  imports: [
+    PassportModule,
+    ConfigModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60m' },
+    }),
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+  ],
+  providers: [
+    AuthService,
+    JwtModule,
+    LocalStrategy,
+    JwtStrategy,
+    KakaoStrategy,
+    GoogleStrategy,
+    NaverStrategy,
+  ],
+  exports: [AuthService, JwtModule, PassportModule],
 })
 export class AuthModule {}
