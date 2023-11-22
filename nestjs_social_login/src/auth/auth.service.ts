06c2118eb459894e1062a3cd5387e3defc942777
Taemin <kwaktaemin98@gmail.com>
2023-11-22 18:13:52 +0900

    추가: 토큰 발급 관련 메서드 추가

--- nestjs_social_login/src/auth/auth.service.ts
+++ nestjs_social_login/src/auth/auth.service.ts
@@ -1,10 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
-import { USER_EXCEPTION } from 'src/common/error/error-code';
+import { JWT_EXCEPTION, USER_EXCEPTION } from 'src/common/error/error-code';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
@@ -57,6 +61,101 @@ export class AuthService {
     if (!user) throw new NotFoundException(USER_EXCEPTION.NOT_FOUND_USER);
 
     return user;
+  }
+
+  async createOnceToken(socialType: string, socialId: string) {
+    const payload = {
+      type: socialType,
+      id: socialId,
+    };
+
+    return await this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_SECRET'),
+      expiresIn: '15m',
+    });
+  }
+
+  async createAccessToken(user: User): Promise<string> {
+    const payload = {
+      type: 'accessToken',
+      id: user.id,
+      nickname: user.nickname,
+    };
+
+    const accessToken = this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_SECRET'),
+      expiresIn: '15m',
+    });
+
+    return accessToken;
+  }
+
+  async createRefreshToken(user: User) {
+    const payload = {
+      type: 'refreshToken',
+      id: user.id,
+      nickname: user.nickname,
+    };
+
+    const token = this.jwtService.sign(payload, {
+      secret: this.configService.get('AES_KEY'),
+      expiresIn: '15m',
+    });
+
+    const tokenVerify = await this.tokenValidate(token);
+    const tokenExp = new Date(tokenVerify['exp'] * 1000);
+
+    const refreshToken = CryptoJS.AES.encrypt(
+      JSON.stringify(token),
+      this.configService.get('AES_KEY'),
+    ).toString();
+
+    const existUser = await this.userRepository.findOne({
+      where: { id: user.id },
+    });
+
+    user.jwtToken = refreshToken;
+    await this.userRepository.save(existUser);
+    return { refreshToken, tokenExp };
+  }
+
+  async reissueRefreshToken(user: User) {
+    const existUser = await this.userRepository.findOne({
+      where: { id: user.id },
+    });
+
+    if (!existUser) throw new NotFoundException(USER_EXCEPTION.NOT_FOUND_USER);
+
+    const payload = {
+      id: user.id,
+      nickname: user.nickname,
+      type: 'refreshToken',
+    };
+
+    const token = this.jwtService.sign(payload, {
+      secret: this.configService.get('JWT_SECRET'),
+      expiresIn: '20700m',
+    });
+
+    const tokenVerify = await this.tokenValidate(token);
+    const tokenExp = new Date(tokenVerify['exp'] * 1000);
+    const current = new Date();
+    const timeRemaining = Math.floor(
+      (tokenExp.getTime() - current.getTime()) / 1000 / 60 / 60,
+    );
+
+    if (timeRemaining > 10)
+      throw new BadRequestException(JWT_EXCEPTION.JWT_NOT_REISSUED);
+
+    const accessToken = await this.createAccessToken(user);
+    const refreshToken = CryptoJS.AES.encrypt(
+      JSON.stringify(token),
+      this.configService.get('AES_KEY'),
+    ).toString();
+
+    existUser.jwtToken = refreshToken;
+    await this.userRepository.save(existUser);
+    return { accessToken, refreshToken: { refreshToken, tokenExp } };
   }
 
   async tokenValidate(token: string) {
