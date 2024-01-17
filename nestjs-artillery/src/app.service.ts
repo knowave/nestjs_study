880158de89b18392519839407459e1cb45dc4670
knowave <kwaktaemin98@gmail.com>
2024-01-17 17:32:00 +0900

    Artillery 명령어를 통한 간단한 테스트를 위한 반복문 추가

--- nestjs-artillery/src/app.service.ts
+++ nestjs-artillery/src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    for (let i = 0; i < 5; i++) {
+      console.log(`쭉쭉 나간다. [${i}]`);
+    }
     return 'Hello World!';
   }
 }
