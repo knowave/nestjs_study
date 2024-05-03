d1ca852bc6cdc1cd1ff0015012a22e09a471b5d7
knowave <kwaktaemin98@gmail.com>
2024-05-03 13:45:49 +0900

    Add UserRepository

--- nestjs_prisma/src/user/user.module.ts
+++ nestjs_prisma/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { UserRepositoryModule } from './repository/user.repository.module';
 
 @Module({
+  imports: [UserRepositoryModule],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
 })
 export class UserModule {}
