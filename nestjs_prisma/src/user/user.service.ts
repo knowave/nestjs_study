42d9ad1e81f61036fbba90e51258792aada421a8
knowave <kwaktaemin98@gmail.com>
2024-05-03 14:03:28 +0900

    회원 가입 및 회원 조회

--- nestjs_prisma/src/user/user.service.ts
+++ nestjs_prisma/src/user/user.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UserRepository } from './repository/user.repository';
+import { User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { EXIST_USER } from './error/user.error';
+import { UserDto } from './repository/dto/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { email, name, password } = createUserDto;
+
+    const existUser = await this.userRepository.getUserByEmail(email);
+    if (existUser) throw new BadRequestException(EXIST_USER);
+
+    const hashedPassword = await this.hashPassword(password);
+    const user = await this.userRepository.save({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    delete user['password'];
+    return user;
+  }
+
+  private async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, process.env.SALT_ROUNDS);
+  }
+
+  async user(userId: number): Promise<UserDto> {
+    const user = await this.userRepository.getUserById(userId);
+    return user;
+  }
+}
