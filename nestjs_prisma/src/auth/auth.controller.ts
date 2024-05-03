07bd56a1ab38b3ec61dd05e1bad26cc4b3c6f0a3
knowave <kwaktaemin98@gmail.com>
2024-05-03 14:06:09 +0900

    Add signup controller

--- nestjs_prisma/src/auth/auth.controller.ts
+++ nestjs_prisma/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private userService: UserService) {}
+
+  @Post('signup')
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUserDto);
+  }
+}
