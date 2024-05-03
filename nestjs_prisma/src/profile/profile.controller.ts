2f2be655650eea52da04d8513f36f8162731bb68
knowave <kwaktaemin98@gmail.com>
2024-05-03 15:46:23 +0900

    프로필 조회 공개

--- nestjs_prisma/src/profile/profile.controller.ts
+++ nestjs_prisma/src/profile/profile.controller.ts
@@ -1,16 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { User } from '@prisma/client';
 import { CreateProfile } from './dto/create-profile.dto';
+import { Public } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('profile')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
-  @Get()
-  async profile(@CurrentUser() user: User) {
-    return this.profileService.profile(user.id);
+  @Get('/:userId')
+  @Public()
+  async profile(@Param('userId') userId: number) {
+    return this.profileService.profile(+userId);
   }
 
   @Post()
