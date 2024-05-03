d7105edc6f274291b389a2be7a278aad14aa3d02
knowave <kwaktaemin98@gmail.com>
2024-05-03 14:04:26 +0900

    generate auth domain

--- nestjs_prisma/src/app.module.ts
+++ nestjs_prisma/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
     }),
     PrismaModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
