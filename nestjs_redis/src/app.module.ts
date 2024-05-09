cd34309c623fc44b1104b647367dc545ae56b4a3
knowave <kwaktaemin98@gmail.com>
2024-05-09 14:03:15 +0900

    CacheModule => RedisModule

--- nestjs_redis/src/app.module.ts
+++ nestjs_redis/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UserModule } from './user/user.module';
 import { MysqlModule } from './user/database/mysql.module';
 import { REDIS_HOST, REDIS_PORT } from 'env';
 import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 
 @Module({
   imports: [
@@ -15,20 +14,16 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
-    CacheModule.register({
-      host: REDIS_HOST,
-      port: REDIS_PORT,
+    RedisModule.forRoot({
+      config: {
+        host: REDIS_HOST,
+        port: +REDIS_PORT,
+      },
     }),
     UserModule,
     MysqlModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
