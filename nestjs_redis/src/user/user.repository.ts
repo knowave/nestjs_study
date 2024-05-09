ecbf3f9cc4d8445136092dfddedb3961e71034cc
knowave <kwaktaemin98@gmail.com>
2024-05-09 13:47:55 +0900

    generate user repository

---
+++ nestjs_redis/src/user/user.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(private readonly dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
+  async saveUser(user: User): Promise<User> {
+    return await this.save(user);
+  }
+
+  async BulkSave(users: User[]): Promise<User[]> {
+    return await this.save(users);
+  }
+
+  async findTopTenUsersAndSortByFollowCount(): Promise<User[]> {
+    return await this.createQueryBuilder('user')
+      .orderBy('user.followCount', 'DESC')
+      .limit(10)
+      .getMany();
+  }
+}
