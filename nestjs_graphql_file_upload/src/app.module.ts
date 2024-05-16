75d454fe9e8c8db648594c24a7d967f0ad92a509
knowave <kwaktaemin98@gmail.com>
2024-05-16 11:17:09 +0900

    generate s3 domain

--- nestjs_graphql_file_upload/src/app.module.ts
+++ nestjs_graphql_file_upload/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UploadModule } from './upload/upload.module';
+import { S3Module } from './s3/s3.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { UploadModule } from './upload/upload.module';
       installSubscriptionHandlers: true,
     }),
     UploadModule,
+    S3Module,
   ],
   controllers: [AppController],
   providers: [AppService],
