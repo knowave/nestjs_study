56395d42306b73da39154c1a60c6848746a27a5e
knowave <kwaktaemin98@gmail.com>
2024-05-16 11:28:50 +0900

    import S3Service

--- nestjs_graphql_file_upload/src/upload/upload.module.ts
+++ nestjs_graphql_file_upload/src/upload/upload.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadResolver } from './upload.resolver';
+import { S3Module } from 'src/s3/s3.module';
 
 @Module({
+  imports: [S3Module],
   providers: [UploadService, UploadResolver],
 })
 export class UploadModule {}
--- nestjs_graphql_file_upload/src/upload/upload.service.ts
+++ nestjs_graphql_file_upload/src/upload/upload.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { S3Service } from 'src/s3/s3.service';
 
 @Injectable()
 export class UploadService {
+  constructor(private readonly s3Service: S3Service) {}
+
   async uploadFile(string: string): Promise<string> {
     return string;
   }
