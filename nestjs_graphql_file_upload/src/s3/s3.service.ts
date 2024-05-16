a7328b1ab5f6063f576ddadc2a492fc4639d641b
knowave <kwaktaemin98@gmail.com>
2024-05-16 11:22:50 +0900

    s3

--- nestjs_graphql_file_upload/src/s3/s3.service.ts
+++ nestjs_graphql_file_upload/src/s3/s3.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
 
 @Injectable()
-export class S3Service {}
+export class S3Service {
+  private readonly s3: S3;
+  private readonly bucketName: string;
+
+  constructor() {
+    this.bucketName = process.env.BUCKET_NAME;
+    this.s3 = new S3({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+      region: process.env.AWS_REGION,
+    });
+  }
+}
