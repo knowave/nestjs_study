37ef1c24f96b849c5c56dce5b9c33480960be7d9
knowave <kwaktaemin98@gmail.com>
2024-05-16 11:22:40 +0900

    Add ConfigModule

--- nestjs_graphql_file_upload/src/app.module.ts
+++ nestjs_graphql_file_upload/src/app.module.ts
@@ -5,9 +5,14 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { UploadModule } from './upload/upload.module';
 import { S3Module } from './s3/s3.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: true,
