2c7eaac27a13b2a50a701bc2f28493114d182cfe
knowave <kwaktaemin98@gmail.com>
2024-05-16 17:29:59 +0900

    수정

--- nestjs_graphql_file_upload/src/upload/interfaces/file-upload.interface.ts
+++ nestjs_graphql_file_upload/src/upload/interfaces/file-upload.interface.ts
@@ -1,8 +1,8 @@
-import { ReadStream } from 'fs';
+import { Stream } from 'stream';
 
 export interface FileUpload {
   filename: string;
   mimetype: string;
   encoding: string;
-  createReadStream(): ReadStream;
+  createReadStream(): Stream;
 }
