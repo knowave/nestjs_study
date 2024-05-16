4be4080ef06e48c6fca38aad8be198f2f7bbe5ca
knowave <kwaktaemin98@gmail.com>
2024-05-16 11:13:27 +0900

    definition graphql module config

--- nestjs_graphql_file_upload/src/app.module.ts
+++ nestjs_graphql_file_upload/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: true,
+      installSubscriptionHandlers: true,
+    }),
+    UploadModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
